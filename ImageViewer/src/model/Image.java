c2afa62dfd2462a9ae5199a922882781daabb017
gonca <gonca@DESKTOP-UT10VI0>
2018-12-03 21:22:27 +0000

    Kata 5 Versión 0.6

--- ImageViewer/src/model/Image.java
+++ ImageViewer/src/model/Image.java
@@ -1,11 +1,10 @@
+
 package model;
 
-import java.awt.Image;
 import java.io.InputStream;
 public interface Image {
     String name();
     InputStream stream();
     Image next();
     Image prev();
-
 }
