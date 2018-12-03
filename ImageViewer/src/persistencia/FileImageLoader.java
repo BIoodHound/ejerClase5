ee70e7dcc15d47f052a2e1aa638c905cd140acff
gonca <gonca@DESKTOP-UT10VI0>
2018-12-03 19:45:40 +0000

    Kata 5 Versión 0.2

--- ImageViewer/src/persistencia/FileImageLoader.java
+++ ImageViewer/src/persistencia/FileImageLoader.java
@@ -1,19 +1,51 @@
 package persistencia;
 
-import java.awt.Image;
-import java.io.File;
-import java.io.FileFilter;
+import java.io.*;
+import model.Image;
 
-public class FileImageLoader {
+public class FileImageLoader implements ImageLoader {
     private final File[] files;
 
-    public FileImageLoader(File[] files) {
-        this.files = files;
+    public FileImageLoader(File folder) {
+        this.files = folder.listFiles(imageType());
+        
     }
     public FileFilter imageType(){
-        return null;
+        return new FileFilter(){
+            @Override
+            public boolean accept(File pathname){
+                return pathname.getName().endsWith(".JPG");
+            }
+        };
     }    
-    public Image imageAt(int i){
-        return null;
+    private Image imageAt(int i){
+        return new Image(){
+            @Override
+            public String name(){
+                return files[i].getName();
+            }
+            @Override
+            public InputStream stream(){
+                try{
+                    return new BufferedInputStream(new FileInputStream(files[i]));
+                } catch (FileNotFoundException e){return null;}
+            }
+            @Override
+            public Image next(){
+                return i == files.length -1 ? imageAt(0) : imageAt(i+1);
+            }
+            @Override
+            public Image prev(){
+                return i == 0 ? imageAt(files.length-1) : imageAt(i-1);
+            }
+
+        };
     }
+
+    @Override
+    public Image Load() {
+        return imageAt(0);
+    }
+
+    
 }
